8370c0bb37705036fe473aedd0363c1fd39981e4
zxb <zxb@MacBook-Pro-2.local>
2025-04-25 09:42:49 +0800

    added jenkins service

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// 创建一个默认的路由引擎
+	r := gin.Default()
+
+	// 添加一个简单的路由
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// 启动服务器，默认监听 8080 端口
+	r.Run(":8080")
+}
